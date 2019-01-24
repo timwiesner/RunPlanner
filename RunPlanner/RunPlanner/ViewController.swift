07889af0435d734fcf2073ef117a0aa45cf9c31e
Tim Wiesner <timothyjwiesner@gmail.com>
2019-01-24 14:58:44 -0600

    Initial commit

---
+++ RunPlanner/RunPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RunPlanner
+//
+//  Created by Tim Wiesner on 1/24/19.
+//  Copyright © 2019 Tim Wiesner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
